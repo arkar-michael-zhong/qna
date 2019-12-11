73ac83ceb8b12e4a009225c1c8be489dda943d0a
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2019-12-12 01:11:44 +0630

    Add votes, answers and views counter to question item

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -9,17 +9,28 @@
 
                 <div class="card-body">
                     @foreach ($questions as $question)
-                        <div class="media">
-                            <div class="media-body">
-                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                <p class="lead">
-                                    Asked by <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                    <small class="text-muted">{{ $question->created_date }}</small>
-                                </p>
-                                {{ Str::limit($question->body, 300) }}
+                    <div class="media">
+                        <div class="d-flex flex-column counters">
+                            <div class="vote">
+                                <strong>{{ $question->votes }}</strong> {{ Str::plural('vote', $question->votes) }}
+                            </div>
+                            <div class="status">
+                                <strong>{{ $question->answers }}</strong> {{ Str::plural('answer', $question->answers) }}
+                            </div>
+                            <div class="view">
+                                {{ $question->views . " " . Str::plural('view', $question->views) }}
                             </div>
                         </div>
-                        <hr>
+                        <div class="media-body">
+                            <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                            <p class="lead">
+                                Asked by <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                <small class="text-muted">{{ $question->created_date }}</small>
+                            </p>
+                            {{ Str::limit($question->body, 300) }}
+                        </div>
+                    </div>
+                    <hr>
                     @endforeach
 
                     <div class="mx-auto">
