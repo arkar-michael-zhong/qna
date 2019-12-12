d1f4e5bc24f76b35b136f18c1fe2e4b68c65d153
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2019-12-12 16:33:43 +0630

    Build Question form

---
+++ resources/views/questions/create.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row justify-content-center ">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                            <h2>Ask Question</h2>
+                            <div class="ml-auto">
+                                <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a>
+                            </div>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <form action="{{ route('questions.store') }}" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label for="question-title" class="col-md-4 col-form-label">Question Title</label>
+                            <input class="form-control @error('title') is-valid @enderror" type="text" name="title" id="question-title" value="{{ old('title', $question->title) }}" autocomplete="title" autofocus>
+                            @error('title')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label for="question-body">Explain your questions here</label>
+                            <textarea id="question-body" class="form-control @error('body') is-valid @enderror" name="body" rows="5" autocomplete="body" autofocus>{{ old('body', $question->body) }}</textarea>
+                            @error('body')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-outline-primary btn-lg">Ask Question</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
