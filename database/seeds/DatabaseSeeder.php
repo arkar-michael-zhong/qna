e834c50d08d901f709617c8a046ead1699e4df48
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2019-12-13 15:14:07 +0630

    Generate fake ansewrs

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,14 +5,18 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+    * Seed the application's database.
+    *
+    * @return void
+    */
     public function run()
     {
         factory(App\User::class, 5)->create()->each(function($u) {
-            $u->questions()->saveMany(factory(App\Question::class, rand(1,5))->make());
+            $u->questions()
+            ->saveMany(factory(App\Question::class, rand(1,5))->make())
+            ->each(function($q) {
+                $q->answers()->saveMany(factory(App\Answer::class, rand(1,5))->make());
+            });
         });
     }
 }
