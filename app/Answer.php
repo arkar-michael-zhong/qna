10b0000724f05e44f1a52fe17cfe64d66d23501b
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2019-12-13 15:57:06 +0630

    display answers for questions

--- app/Answer.php
+++ app/Answer.php
@@ -11,12 +11,12 @@ class Answer extends Model
         return $this->belongsTo(Question::class);
     }
 
-    public function userr()
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-        public function getBodyHtmlAttribute()
+    public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
     }
@@ -29,5 +29,10 @@ class Answer extends Model
             $answer->question->increment('answers_count');
             $answer->question->save();
         });
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
     }
 }
--- app/User.php
+++ app/User.php
@@ -11,28 +11,28 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    * The attributes that should be hidden for arrays.
+    *
+    * @var array
+    */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
     /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
+    * The attributes that should be cast to native types.
+    *
+    * @var array
+    */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
@@ -52,5 +52,14 @@ class User extends Authenticatable
     public function answers()
     {
         return $this->hasMany(Answer::class);
+    }
+
+    // GENERATE AVATOR
+    public function getAvatarAttribute()
+    {
+        $email = $this->email;
+        $size = 32;
+
+        return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "&s=" . $size;
     }
 }
