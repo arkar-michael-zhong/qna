deed6c2ad35a691d5a1f976cc64de3fba190290e
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2019-12-14 22:24:01 +0630

    tidy up the view

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -11,49 +11,7 @@
                 @include('layouts._messages')
 
                 @foreach ($answers as $answer)
-
-                {{-- VOTE-CONTROLS AND CREATED_DATE-AND-AVATER --}}
-                <div class="media">
-                    {{-- VOTE-CONTROLS --}}
-                    @include('shared._vote', [
-                    'model' => $answer
-                    ])
-                    {{-- //VOTE-CONTROLS END --}}
-
-                    {{-- CREATED_DATE-AND-AVATER --}}
-                    <div class="media-body">
-                        {!! $answer->body_html !!}
-                        <div class="row">
-                            <div class="col-4">
-                                <div class="ml-auto">
-                                    @can ('update', $answer)
-                                    <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">ပြင်မယ်</a>
-                                    @endcan
-
-                                    @can ('delete', $answer)
-                                    <form class="form-delete" method="post" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('မင်းဖျတ်မှာသေချာလား?')">ဖျက်မယ်</button>
-                                    </form>
-                                    @endcan
-                                </div>
-                            </div>
-
-                            <div class="col-4"></div>
-
-                            <div class="col-4">
-                                @include('shared._author', [
-                                'model' => $answer,
-                                'label' => 'answered'
-                                ])
-                            </div>
-                        </div>
-                    </div>
-                    {{-- //CREATED_DATE-AND-AVATER END --}}
-                </div>
-                {{-- //VOTE-CONTROLS AND CREATED_DATE-AND-AVATER END --}}
-                <hr>
+                @include('answers._answer')
                 @endforeach
             </div>
         </div>
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -18,54 +18,13 @@
                     @include('layouts._messages')
 
                     @forelse ($questions as $question)
-                    <div class="media">
-                        <div class="d-flex flex-column counters">
-                            <div class="vote">
-                                <strong>{{ $question->votes_count }}</strong> {{ Str::plural('vote', $question->votes_count) }}
-                            </div>
-                            <div class="status {{ $question->status }}">
-                                <strong>{{ $question->answers_count }}</strong> {{ Str::plural('answer', $question->answers_count) }}
-                            </div>
-                            <div class="view">
-                                {{ $question->views . " " . Str::plural('view', $question->views) }}
-                            </div>
-                        </div>
-                        <div class="media-body">
-                            <div class="d-flex align-items-center">
-                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                <div class="ml-auto">
-                                    @can ('update', $question)
-                                    <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">ပြင်မယ်</a>
-                                    @endcan
-
-                                    @can ('delete', $question)
-                                    <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('မင်းဖျတ်မှာသေချာလား?')">ဖျက်မယ်</button>
-                                    </form>
-                                    @endcan
-                                </div>
-                            </div>
-                            <p class="lead">
-                                Asked by <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                <small class="text-muted">{{ $question->created_date }}</small>
-                            </p>
-                            <div class="excerpt">
-                                {{ $question->excerpt(400) }}
-                            </div>
-                        </div>
-                    </div>
-                    <hr>
+                    @include('questions._excerpt')
                     @empty
                     <div class="alert alert-warning">
                         <h1><strong>ဆောတီးပဲ 😎</strong> မင်းအတွက်မေးခွန်းမရှိဘူး. ဒိုးတော့ 🤬</h1>
                     </div>
                     @endforelse
-
-                    <div class="mx-auto">
-                        {{ $questions->links() }}
-                    </div>
+                    {{ $questions->links() }}
                 </div>
             </div>
         </div>
